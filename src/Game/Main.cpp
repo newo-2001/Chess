be49b4c3de52d3444ae65a7a5182737d92982858
newo-2001 <o.elderbroek@gmail.com>
2023-03-17 17:51:22 +0100

    Project Structure

---
+++ src/Game/Main.cpp
@@ -0,0 +1,8 @@
+#include <pch.h>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
---
+++ src/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <GL/glew.h>
+
+#include <glm/glm.hpp>
+
+#include <vector>
+#include <array>
+#include <string>
+#include <unordered_map>
+
+#include <iostream>
+#include <memory>
+#include <stdexcept>
